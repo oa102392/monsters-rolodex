dd8a905d9a8f15a862485b5f9c3f1920dc538560
oa102392 <oalba@csumb.edu>
2019-11-27 01:33:23 -0800

    Updates

---
+++ precache-manifest.4cd05e97191aebbd1e8eb7aa606b60cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aae24a1ca84c2b38dc542d0c1889a4f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "95e71e5f2482045f784d",
+    "url": "/monsters-rolodex/static/css/main.d4ede290.chunk.css"
+  },
+  {
+    "revision": "3b7baa3b68dae5577849",
+    "url": "/monsters-rolodex/static/js/2.b70bcd0b.chunk.js"
+  },
+  {
+    "revision": "95e71e5f2482045f784d",
+    "url": "/monsters-rolodex/static/js/main.6aa2bf15.chunk.js"
+  },
+  {
+    "revision": "3d30865087a4d44a0b65",
+    "url": "/monsters-rolodex/static/js/runtime-main.6a02ae69.js"
+  }
+]);
